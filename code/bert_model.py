f88e31bdb592936c1b27523a8180a925faf9ddd7
Luka Bračun <luka@proxima.si>
2021-04-28 13:03:22 +0200

    use custom bert model

--- code/bert_model.py
+++ code/bert_model.py
@@ -4,28 +4,40 @@ import numpy as np
 from sklearn import preprocessing
 from sklearn.base import BaseEstimator
 from sklearn.model_selection import train_test_split
-from transformers import TFBertForSequenceClassification, BertTokenizer
+from transformers import BertTokenizer, TFBertModel, TFBertPreTrainedModel, TFBertMainLayer
+from tensorflow.keras import layers
 
 
 
 class BertModel(BaseEstimator):
-    def __init__(self, train=False):
+    def __init__(self, output_classes=15, dropout_rate=0.2, cnn_filters=100, dnn_units=256, epochs=5):
         # We have to set the parameters in __init__ for cloning.
-        self.train = train
+        self.epochs = epochs
+        self.cnn_filters = cnn_filters
+        self.dropout_rate = dropout_rate
+        self.output_classes = output_classes
+        self.dnn_units = dnn_units
+         
+        # set tokenizer
+        self._tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
         # set model
-        # download from here: https://drive.google.com/file/d/1DblEPtyKZ0olTPe1tG_dZUWxOPJs6Oen/view?usp=sharing
-        # or train yourself -> use bert.ipynb
-        self._model = TFBertForSequenceClassification.from_pretrained('../models/bert/', num_labels=15)
-        
-        # set tokenizer
-        self._tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+        self._model = BertCustomEmbeddingModel.from_pretrained('bert-base-uncased', 
+                                cnn_filters=self.cnn_filters,
+                                dnn_units=self.dnn_units,
+                                model_output_classes=self.output_classes,
+                                dropout_rate=self.dropout_rate)
 
-        # compile model
-        optimizer = tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0)
-        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
-        self._model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+        if output_classes == 2:
+            self._model.compile(loss="binary_crossentropy",
+                                optimizer="adam",
+                                metrics=["accuracy"])
+        else:
+            self._model.compile(loss="sparse_categorical_crossentropy",
+                                optimizer="adam",
+                                metrics=["sparse_categorical_accuracy"])
+
+        
 
         # set LabelEncoder for encoding/decoding target values string<->number
         self.label_encoder = preprocessing.LabelEncoder()
@@ -36,21 +48,15 @@ class BertModel(BaseEstimator):
     def fit(self, messages, target):
         # encode targets, so they can be decoded later
         target = self.label_encoder.fit_transform(target)
-    
-        # no need to train, because we pretrained it (model in /models/bert)
-        if self.train:
-            # split to train and validation data
-            X_train, X_val, y_train, y_val = train_test_split(messages, target, test_size=0.2, random_state=13)
 
-            # tokenize data
-            X_train_input = self.convert_to_input(X_train)
-            X_val_input = self.convert_to_input(X_val)
+        # tokenize data
+        train_token_ids = self.get_token_ids(messages)
 
-            # create tf.dataset
-            train_ds = tf.data.Dataset.from_tensor_slices((X_train_input[0],X_train_input[1],X_train_input[2],y_train)).map(self.example_to_features).shuffle(100).batch(12).repeat(5)
-            val_ds=tf.data.Dataset.from_tensor_slices((X_val_input[0],X_val_input[1],X_val_input[2],y_val)).map(self.example_to_features).batch(12)
+        # create tf dataset
+        train_data = tf.data.Dataset.from_tensor_slices((tf.constant(train_token_ids), tf.constant(target))).batch(12)
 
-            self._model.fit(train_ds, epochs=3, validation_data=val_ds)
+        # train model
+        self._model.fit(train_data, epochs=self.epochs)
 
         return self
 
@@ -63,40 +69,71 @@ class BertModel(BaseEstimator):
 
     def predict_proba(self, messages):
         # tokenize data
-        X_input = self.convert_to_input(messages)
-        # create tf.dataset
-        X_ds = tf.data.Dataset.from_tensor_slices((X_input[0],X_input[1],X_input[2], np.ones(len(messages)))).map(self.example_to_features).batch(12)
+        test_token_ids = self.get_token_ids(messages)
 
-        return self._model.predict(X_ds).logits
+        # create tf dataset
+        test_data = tf.data.Dataset.from_tensor_slices((tf.constant(test_token_ids), tf.constant(np.ones(len(messages))))).batch(12)
 
-
-    # from labs 09 - Transformers and BERT
-    def convert_to_input(self, messages, pad_token=0, pad_token_segment_id=0, max_length=128):
-        input_ids, attention_masks,token_type_ids=[],[],[]
-
-        for message in messages:
-            inputs = self._tokenizer.encode_plus(message, add_special_tokens=True, max_length=max_length)
-
-            i, t = inputs["input_ids"], inputs["token_type_ids"]
-            m = [1] * len(i)
-
-            padding_length = max_length - len(i)
-
-            i = i + ([pad_token] * padding_length)
-            m = m + ([0] * padding_length)
-            t = t + ([pad_token_segment_id] * padding_length)
-            
-            input_ids.append(i)
-            attention_masks.append(m)
-            token_type_ids.append(t)
-
-        return [np.asarray(input_ids), 
-                np.asarray(attention_masks), 
-                np.asarray(token_type_ids)]
-
+        # run model prediction
+        return self._model.predict(test_data)
 
     # from labs 09 - Transformers and BERT
-    def example_to_features(self, input_ids, attention_masks, token_type_ids, y):
-        return {"input_ids": input_ids,
-                "attention_mask": attention_masks,
-                "token_type_ids": token_type_ids},y
+    def get_token_ids(self, texts):
+        return self._tokenizer.batch_encode_plus(texts, 
+                                            add_special_tokens=True, 
+                                            max_length = 128, 
+                                            pad_to_max_length = True)["input_ids"]
+
+
+# from labs 09 - Transformers and BERT
+class BertCustomEmbeddingModel(TFBertPreTrainedModel):
+    def __init__(self, config,
+                 cnn_filters=50,
+                 dnn_units=512,
+                 model_output_classes=2,
+                 dropout_rate=0.1,
+                 training=False,
+                 name="text_model",
+                 *inputs, **kwargs):
+        super().__init__(config, *inputs, **kwargs)
+        self.bert = TFBertMainLayer(config, name="bert", trainable = False)
+        
+        self.cnn_layer1 = layers.Conv1D(filters=cnn_filters,
+                                        kernel_size=2,
+                                        padding="valid",
+                                        activation="relu")
+        self.cnn_layer2 = layers.Conv1D(filters=cnn_filters,
+                                        kernel_size=3,
+                                        padding="valid",
+                                        activation="relu")
+        self.cnn_layer3 = layers.Conv1D(filters=cnn_filters,
+                                        kernel_size=4,
+                                        padding="valid",
+                                        activation="relu")
+        self.pool = layers.GlobalMaxPool1D()
+        
+        self.dense_1 = layers.Dense(units=dnn_units, activation="relu")
+        self.dropout = layers.Dropout(rate=dropout_rate)
+        if model_output_classes == 2:
+            self.last_dense = layers.Dense(units=1,
+                                           activation="sigmoid")
+        else:
+            self.last_dense = layers.Dense(units=model_output_classes,
+                                           activation="softmax")
+
+    def call(self, inputs, training = False, **kwargs):        
+        bert_outputs = self.bert(inputs, training = training, **kwargs)
+        
+        l_1 = self.cnn_layer1(bert_outputs[0]) 
+        l_1 = self.pool(l_1) 
+        l_2 = self.cnn_layer2(bert_outputs[0]) 
+        l_2 = self.pool(l_2)
+        l_3 = self.cnn_layer3(bert_outputs[0])
+        l_3 = self.pool(l_3) 
+        
+        concatenated = tf.concat([l_1, l_2, l_3], axis=-1) # (batch_size, 3 * cnn_filters)
+        concatenated = self.dense_1(concatenated)
+        concatenated = self.dropout(concatenated, training)
+        model_output = self.last_dense(concatenated)
+        
+        return model_output
