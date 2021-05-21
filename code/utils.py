21abf60dcf4cb85fcb5f0f8e7e4a11fbbc76d634
klavdijav <kv8904@student.uni-lj.si>
2021-05-21 14:09:40 +0200

    custom features

--- code/utils.py
+++ code/utils.py
@@ -4,6 +4,8 @@ from typing import Tuple
 
 import contractions
 import nltk
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
 import numpy as np
 import pandas as pd
 from sklearn import model_selection
@@ -15,6 +17,11 @@ COL_COLLAB_RESP = "Collab Response"
 COL_PSEUDONYM = "Pseudonym"
 COL_BOOKCLUB = "Bookclub"
 COL_RESP_NUMBER = "Response Number"
+COL_BOOKID = "Book ID"
+COL_MESSAGE_LEN = "Message Length"
+COL_BOOK_SIMILARITY = "Book Similarity"
+COL_CONTAINS_LINK = "Contains Link"
+COL_CONTAINS_EMOTICON = "Contains Emoticon"
 
 COL_TARGET = "CodePreliminary"
 
@@ -43,10 +50,15 @@ def load_discussions_data(keep_punctuation=True) -> Tuple[pd.DataFrame, pd.Serie
     joined_df = df1.append(df2).join(resp_df, on=COL_RESP_NUMBER)
 
     target_df = preprocess_target(joined_df[COL_TARGET])
-    data_df = joined_df[[COL_MESSAGE, COL_BOOKCLUB, COL_PSEUDONYM, COL_RESP_NUMBER, COL_COLLAB_RESP]].copy()
+    data_df = joined_df[[COL_MESSAGE, COL_BOOKID, COL_BOOKCLUB, COL_PSEUDONYM, COL_RESP_NUMBER, COL_COLLAB_RESP]].copy()
 
+    data_df[COL_CONTAINS_EMOTICON] = data_df.apply(emoticons_feature, axis=1)
+    data_df[COL_CONTAINS_LINK] = data_df.apply(has_link_feature, axis=1)
     data_df.loc[:, COL_MESSAGE] = preprocess_text(data_df[COL_MESSAGE], keep_punctuation)
     data_df.loc[:, COL_COLLAB_RESP] = preprocess_text(data_df[COL_COLLAB_RESP], keep_punctuation)
+    data_df[COL_BOOK_SIMILARITY] = data_df.apply(message_book_similarity, axis=1)
+
+    print(data_df)
 
     return data_df, target_df
 
@@ -92,3 +104,60 @@ def cross_validate_model(model, data_df, target_df, n_splits=4, random_state=1, 
         scoring=("accuracy", "f1_macro", "neg_log_loss"),
     )
     return scores
+
+def preprocess_book(title: string):
+    path = "../data/books/" + title
+    with open(path, 'r') as book: 
+        book = book.read().replace('“', '"').replace('”', '"').replace("’", "'").replace("—", "-").replace("…", "")
+        text = book.lower()
+
+        table = text.maketrans({key: "" for key in string.punctuation})
+        text = text.translate(table)
+
+        tokens = nltk.word_tokenize(text)
+        
+
+        tokens = [token for token in tokens if token not in stopwords.words("english")]
+        #freq = FreqDist(tokens)
+        #print(len(tokens))
+        #freq.plot(20)
+
+        tokens_filtered = []
+        for token in tokens: 
+            if token not in tokens_filtered:
+                tokens_filtered.append(token)
+        
+        return tokens_filtered
+
+
+book1 = preprocess_book("Design_for_the_Future_When_the_Future_Is_Bleak.txt")
+book2 = preprocess_book("Just_Have_Less.txt")
+book3 = preprocess_book("The_Lady_or_the_Tiger.txt")
+
+def message_book_similarity(row): 
+    message = row[COL_MESSAGE].split(" ")
+    bookid = row [COL_BOOKID]
+    book = None
+    
+    if (bookid == 260 or bookid == 261):
+        book = book3
+    elif (bookid == 266 or bookid == 267): 
+        book = book1
+    else:
+        book = book2
+
+    return sum([m in book for m in message])
+
+def word_count_feature(row):
+    return len(row[COL_MESSAGE].split(" "))
+
+def char_count_feature(row):
+    return len(row[COL_MESSAGE])
+
+def has_link_feature(row):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    return 1 if re.match(regex, row[COL_MESSAGE]) else 0
+
+def emoticons_feature(row):
+    regex = r"(\:\w+\:|\<[\/\\]?3|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)"
+    return 1 if re.match(regex, row[COL_MESSAGE]) else 0
